753693bc74e1465d37eed794505427f093dd516c
Claudia Ziemba <claudia.ziemba@urbetrack.com>
2023-05-18 23:12:21 -0300

    cleaning project

--- modulo_03/Unidad_4_js/ejercicio_2/ejercicio_2.js
+++
@@ -1,9 +0,0 @@
-// Programar un contador de caracteres para un textarea.
-
-const button = document.getElementById("counter_amount");
-const result = document.getElementById("counter");
-const write = document.getElementById("write");
-
-button.addEventListener('click',function() {
-	result.innerHTML = write.value.length;	
-});
